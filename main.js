80f8c92cfea05fd42b5a4f48d186add594cd3c8b
wiz64 <67432394+wiz64@users.noreply.github.com>
2022-08-04 08:48:59 +0000

    optimise

--- main.js
+++ main.js
@@ -14,19 +14,22 @@ require('dotenv').config();
 // Greet the user
 var greet = ` [GREET] Hello, I'm ${stacy_info.name} ! I am ${stacy_info.birth.age}old. You can also call me ${quic.randomFromList(stacy_info.aliases)}.`
 console.log(greet)
+
+console.log("Version : " + stacy_info.version)
+
+// if telegram bot interface is enabled, start it
 if(process.env.USE_TELEGRAM == 1) {
 console.log(" [INFO] Starting Telegram Interface ...")
-// RUN TELEGRAM SCRIPT
 interfaces.start('node','interfaces/telegram.js')
 } else { console.log(" [INFO] Telegram Interface is Disabled") }
 
+// if Stacy Web Interface is enabled, start it
 if(process.env.USE_WEB == 1) {
-// express hello world listen on port 3000
 console.log(" [INFO] Starting Web Service ...")
 interfaces.start('node','interfaces/web.js')
 } else { console.log(" [INFO] Web Service is Disabled") }
 
-// if discord is enabled, start the discord interface
+// if discord bot interface is enabled, start it
 if(process.env.USE_DISCORD == 1) {
 console.log(" [INFO] Starting Discord Interface ...")
 interfaces.start('node','interfaces/discord.js')
@@ -36,6 +39,7 @@ var cleanExit = function() {
     interfaces.killAll();
     process.exit();
 };
-//process.on('exit', cleanExit);
+
+process.on('exit', cleanExit);
 process.on('SIGINT', cleanExit); // catch ctrl-c
 process.on('SIGTERM', cleanExit); // catch kill
